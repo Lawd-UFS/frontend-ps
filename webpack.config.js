ffcdcbd12b65bc5195130af4dfeaf2e048abfeb5
Davi Bittencourt <davi.almeida@dcomp.ufs.br>
2025-01-16 22:18:09 -0300

    build: adiciona configuração para carregar imagens diretamente no html

--- webpack.config.js
+++ webpack.config.js
@@ -72,6 +72,10 @@ const config = {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
         type: 'asset',
       },
+      {
+        test: /\.html$/i,
+        use: 'html-loader',
+      },
     ],
   },
   mode: 'development',
