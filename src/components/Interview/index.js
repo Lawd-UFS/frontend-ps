a46e0fc31db739eabc6b6c29cf3e80d24b46ba07
Davi Bittencourt <davi.almeida@dcomp.ufs.br>
2025-01-31 19:21:19 -0300

    feat: adiciona exportação de componentes da entrevista

---
+++ src/components/Interview/index.js
@@ -0,0 +1,7 @@
+import { InterviewInstructions } from './Instructions';
+import { InterviewStages } from './Stages';
+
+export const Interview = {
+  Instructions: InterviewInstructions,
+  Stages: InterviewStages,
+};
