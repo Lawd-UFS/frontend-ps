b6327c70ee7a48c712be6392ca5be5a1a89b5da0
Davi Bittencourt <davi.almeida@dcomp.ufs.br>
2025-01-31 19:13:22 -0300

    feat: adiciona componente de etapas da entrevista

---
+++ src/components/Interview/Stages/index.js
@@ -0,0 +1,32 @@
+import './styles.css';
+
+import circleSVG from '../../../assets/images/circle.svg';
+
+export const InterviewStages = (stages = []) => {
+  const div = document.createElement('div');
+  div.className = 'stages';
+
+  div.innerHTML = `
+      <h2>Etapas</h2>
+      `;
+
+  const nav = document.createElement('nav');
+
+  stages.forEach((stage) => {
+    const a = document.createElement('a');
+    a.className = 'stage';
+
+    a.innerHTML = `
+    ${circleSVG}
+    <p>${stage}</p>
+    `;
+
+    nav.appendChild(a);
+  });
+
+  div.appendChild(nav);
+
+  div.querySelector('.stage').setAttribute('active', '');
+
+  return div;
+};
