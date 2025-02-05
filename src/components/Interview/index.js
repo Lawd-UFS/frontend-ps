8876ed9443aa0b04a269b4913322b161f7fc3f19
Davi Bittencourt <davi.almeida@dcomp.ufs.br>
2025-02-05 11:40:23 -0300

    feat: adiciona componente de horário de entrevista

--- src/components/Interview/index.js
+++ src/components/Interview/index.js
@@ -1,11 +1,13 @@
 import { InterviewInstructions } from './Instructions';
 import { InterviewQuestions } from './Questions';
 import { InterviewStages } from './Stages';
+import { InterviewTime } from './Time';
 
 const Interview = {
   Instructions: InterviewInstructions,
   Questions: InterviewQuestions,
   Stages: InterviewStages,
+  Time: InterviewTime,
 };
 
 export default Interview;
