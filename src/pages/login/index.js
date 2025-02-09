658a6ef9aa09ee5c0613eb2d5960a7cfc40c9a3e
Jessica <jessicaviana.dev@gmail.com>
2025-02-08 22:02:05 -0300

    fix: primeira parte de ajustes no login

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,14 +1,16 @@
 import './index.css';
 
+const password = document.getElementById('password');
+const visibility = document.getElementById('visibility');
 //função de clique para mostrar e esconder a senha.
 const visibilityPassword = () => {
-  const password = document.getElementById('password');
-  const visibility = document.getElementById('visibility');
   if (password.type == 'password') {
     password.type = 'text';
-    visibility.src = '../images/visibility.png';
+    visibility.src = '../../assets/images/visibility.png';
   } else {
     password.type = 'password';
-    visibility.src = '../images/visibility_off.png';
+    visibility.src = '../../assets/images/visibility-off.png';
   }
 };
+
+visibility.addEventListener('click', visibilityPassword);
