579c99c3f743b710ec76dc5e9b30b6d5bad9dff6
Davi Bittencourt <davi.almeida@dcomp.ufs.br>
2025-02-23 15:41:02 -0300

    feat: adiciona lógica de gerenciamento de ui a depender da resposta da confirmação
    Para o caso onde o token é expirado, é renderizado o componente com a opção de input para reenvio de email, mas caso o token não seja informado, é apenas renderizado uma ui genérica de erro

--- src/pages/email-confirmation/index.js
+++ src/pages/email-confirmation/index.js
@@ -1,1 +1,43 @@
+import EnrollmentSubmission from '../../components/EnrollmentSubmission';
+import { HttpClient } from '../../infra/http/httpClient';
+import { EmailService } from '../../service/EmailService';
 import './index.css';
+
+const main = document.querySelector('main');
+
+const emailService = new EmailService(HttpClient.create());
+
+const getUrlToken = () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get('token');
+};
+
+document.addEventListener('DOMContentLoaded', async () => {
+  const token = getUrlToken();
+
+  if (!token) {
+    main.appendChild(EnrollmentSubmission.Error({ hasToken: false }));
+    return;
+  }
+
+  const result = await emailService.confirmEmail(token);
+
+  if (result.success) {
+    main.appendChild(EnrollmentSubmission.Success());
+    return;
+  }
+
+  if (result.message === 'Token inválido ou expirado.') {
+    main.appendChild(
+      EnrollmentSubmission.Error({
+        hasToken: true,
+        resendEmail: emailService.resendEmail,
+        message:
+          'Parece que a sua confirmação expirou. Deseja reenviar o email?',
+      }),
+    );
+    return;
+  }
+
+  main.appendChild(EnrollmentSubmission.Error({ hasToken: true }));
+});
