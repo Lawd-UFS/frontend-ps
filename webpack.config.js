7d49829969f1b7784e1bb5416259ea97b5f763c1
Davi Bittencourt <davi.almeida@dcomp.ufs.br>
2025-02-23 21:58:36 -0300

    fix: ajusta webpack para renderizar páginas na rota padrão

--- webpack.config.js
+++ webpack.config.js
@@ -20,8 +20,6 @@ const generatePages = (root = '', dir = pagesPath) => {
     const relativePath = path.join(root, file);
     const fileStat = fs.statSync(fullPath);
 
-    if (readDir === dir && !fileStat.isDirectory()) return;
-
     if (fileStat.isDirectory()) {
       const { entries: subEntries, htmlPlugins: subHtmlPlugins } =
         generatePages(relativePath);
