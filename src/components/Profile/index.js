3aa29f9837e36603922501e0f9980fa9ffe12bef
Davi Bittencourt <davi.almeida@dcomp.ufs.br>
2025-02-27 01:29:31 -0300

    fix: corrige verificação para carregamento da imagem

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -2,16 +2,25 @@ import './styles.css';
 import genericUserProfileImg from '../../assets/images/icon-user.png';
 
 export const Profile = ({ name, imgSrc }) => {
-  const img = imgSrc ?? genericUserProfileImg;
-
   const profile = document.createElement('div');
-
   profile.setAttribute('class', 'profile');
 
-  profile.innerHTML = `
-    <img src="${img}" alt="Foto de perfil" />
+  const imgElement = new Image();
+  imgElement.src = imgSrc;
+
+  imgElement.onload = () => {
+    profile.innerHTML = `
+    <img src="${imgSrc}" alt="Foto de perfil" />
     <span class="name-user">${name}</span>
   `;
+  };
+
+  imgElement.onerror = () => {
+    profile.innerHTML = `
+    <img src="${genericUserProfileImg}" alt="Foto de perfil" />
+    <span class="name-user">${name}</span>
+  `;
+  };
 
   return profile;
 };
