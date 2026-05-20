1561d545a9e4ce95a05a553a46db3b3efb2a7b3b
Bruno Saint Clair <brunosaint@dcomp.ufs.br>
2026-05-20 17:16:03 -0300

    feat: atualiza countdown

--- src/pages/home/states/countdown/index.js
+++ src/pages/home/states/countdown/index.js
@@ -1,6 +1,6 @@
 import './index.css';
 
-const inicioPS = new Date('2025-02-24T00:00:00');
+const inicioPS = new Date('2026-06-01T00:00:00');
 
 function count() {
   const dataSystem = new Date();
@@ -11,15 +11,32 @@ function count() {
   const minutesT = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
   const secondsT = Math.floor((time % (1000 * 60)) / 1000);
 
-  document.getElementById('day').textContent = daysT < 10 ? `0${daysT}` : daysT;
-  document.getElementById('hours').textContent =
-    hoursT < 10 ? `0${hoursT}` : hoursT;
-  document.getElementById('minutes').textContent =
-    minutesT < 10 ? `0${minutesT}` : minutesT;
-  document.getElementById('seconds').textContent =
-    secondsT < 10 ? `0${secondsT}` : secondsT;
+  if (time > 0) {
+    document.getElementById('day').textContent = daysT < 10 ? `0${daysT}` : daysT;
+    document.getElementById('hours').textContent =
+      hoursT < 10 ? `0${hoursT}` : hoursT;
+    document.getElementById('minutes').textContent =
+      minutesT < 10 ? `0${minutesT}` : minutesT;
+    document.getElementById('seconds').textContent =
+      secondsT < 10 ? `0${secondsT}` : secondsT;
 
-  if (time > 0) setTimeout(count, 1000);
+    setTimeout(count, 1000);
+  } else {
+    document.getElementById('day').textContent = '00';
+    document.getElementById('hours').textContent = '00';
+    document.getElementById('minutes').textContent = '00';
+    document.getElementById('seconds').textContent = '00';
+
+    const cdMessageDiv = document.querySelector('.cd-message');
+    if (cdMessageDiv) {
+      cdMessageDiv.classList.add('opened');
+    }
+
+    const cdMessage = document.querySelector('.cd-message h1');
+    if (cdMessage) {
+      cdMessage.innerHTML = 'As inscrições já abriram!';
+    }
+  }
 }
 
 count();
